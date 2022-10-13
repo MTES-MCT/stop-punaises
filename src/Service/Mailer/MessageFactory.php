fe8692a09b577647af4da144e764edcf03177159
Saidi <saidi.ahamada@gmail.com>
2022-10-13 12:45:34 +0200

    fix link mail

--- src/Service/Mailer/MessageFactory.php
+++ src/Service/Mailer/MessageFactory.php
@@ -4,12 +4,16 @@ namespace App\Service\Mailer;
 
 class MessageFactory
 {
-    public function __construct(private string $from)
+    public function __construct(private string $baseUrl, private string $from)
     {
     }
 
     public function createInstanceFrom(Template $template, array $parameters = [])
     {
+        if (isset($parameters['link'])) {
+            $parameters['link'] = $this->baseUrl.$parameters['link'];
+        }
+
         return (new Message())
             ->setFrom($this->from)
             ->setTemplate($template)
