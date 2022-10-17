e4c42adce4f0365d9f88a37b36e8930b54e3c066
Saidi <saidi.ahamada@gmail.com>
2022-10-17 13:54:28 +0200

    apply debug

--- scripts/postdeploy.sh
+++ scripts/postdeploy.sh
@@ -5,7 +5,7 @@ php bin/console doctrine:migrations:migrate --no-interaction
 
 if [[ -z "${COMPOSER_DEV}" ]];
 then
-    echo "No data fixtures to load!!!"
+    echo "No data fixtures to load!!"
 else
     echo "Load data fixtures..."
     composer dump-env dev && php bin/console -e dev doctrine:fixtures:load --no-interaction
