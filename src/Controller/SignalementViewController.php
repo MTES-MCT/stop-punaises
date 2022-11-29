2c2c5d6e4dd135802bd1f39f63d4c012ceb16610
emilschn <emilien.schneider@beta.gouv.fr>
2022-11-29 17:15:33 +0100

    wip signalement entreprise

--- src/Controller/SignalementViewController.php
+++ src/Controller/SignalementViewController.php
@@ -14,18 +14,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class SignalementViewController extends AbstractController
 {
     #[Route('/bo/signalements/{uuid}', name: 'app_signalement_view')]
-    public function index(Signalement $signalement): Response
+    public function indexSignalement(Signalement $signalement): Response
     {
         if (!$signalement) {
             return $this->render('signalement_view/not-found.html.twig');
-        }
-        // Not the admin
-        // and the signalement is linked to an Entreprise
-        // but its Entreprise is different than the one from the user
-        if (!$this->isGranted('ROLE_ADMIN')
-            && null !== $signalement->getEntreprise()
-            && $signalement->getEntreprise()->getId() !== $this->getUser()->getEntreprise()->getId()) {
-            return $this->render('signalement_view/not-authorized.html.twig');
         }
 
         $signalementPhotos = $signalement->getPhotos();
@@ -42,13 +34,57 @@ class SignalementViewController extends AbstractController
             ];
         }
 
-        return $this->render('signalement_view/index.html.twig', [
+        return $this->render('signalement_view/signalement.html.twig', [
+            'can_display_traitement' => false, // TODO
+            'can_display_adresse' => false, // TODO
             'signalement' => $signalement,
-            'photos' => $photos,
+            'photos' => $this->getPhotos($signalement),
         ]);
     }
 
-    #[Route('/bo/signalements/{uuid}/ajouter-photos', name: 'app_add_photos')]
+    #[Route('/bo/historique/{uuid}', name: 'app_signalement_historique_view')]
+    public function indexHistorique(Signalement $signalement): Response
+    {
+        if (!$signalement) {
+            return $this->render('signalement_view/not-found.html.twig');
+        }
+        // Not the admin
+        // and the signalement is linked to an Entreprise
+        // but its Entreprise is different than the one from the user
+        if (!$this->isGranted('ROLE_ADMIN')
+            && null !== $signalement->getEntreprise()
+            && $signalement->getEntreprise()->getId() !== $this->getUser()->getEntreprise()->getId()) {
+            return $this->render('signalement_view/not-authorized.html.twig');
+        }
+
+        return $this->render('signalement_view/historique.html.twig', [
+            'can_display_traitement' => true,
+            'can_display_adresse' => true,
+            'signalement' => $signalement,
+            'photos' => $this->getPhotos($signalement),
+        ]);
+    }
+
+    private function getPhotos(Signalement $signalement)
+    {
+        $signalementPhotos = $signalement->getPhotos();
+        $photos = [];
+        foreach ($signalementPhotos as $signalementPhoto) {
+            $photos[] = [
+                'url' => $this->generateUrl(
+                    'show_uploaded_file',
+                    ['filename' => $signalementPhoto['file']]
+                ),
+                'file' => $signalementPhoto['file'],
+                'title' => $signalementPhoto['title'],
+                'date' => $signalementPhoto['date'],
+            ];
+        }
+
+        return $photos;
+    }
+
+    #[Route('/bo/historique/{uuid}/ajouter-photos', name: 'app_add_photos')]
     public function addPhoto(
         Signalement $signalement,
         Request $request,
@@ -65,10 +101,10 @@ class SignalementViewController extends AbstractController
         $signalement->setPhotos($filesToSave);
         $signalementManager->save($signalement);
 
-        return $this->redirectToRoute('app_signalement_view', ['uuid' => $signalement->getUuid()]);
+        return $this->redirectToRoute('app_signalement_historique_view', ['uuid' => $signalement->getUuid()]);
     }
 
-    #[Route('/bo/signalements/{uuid}/{filename}/supprimer-photo', name: 'app_delete_photo')]
+    #[Route('/bo/historique/{uuid}/{filename}/supprimer-photo', name: 'app_delete_photo')]
     public function deletePhoto(
         Signalement $signalement,
         string $filename,
@@ -91,6 +127,6 @@ class SignalementViewController extends AbstractController
             $signalementManager->save($signalement);
         }
 
-        return $this->redirectToRoute('app_signalement_view', ['uuid' => $signalement->getUuid()]);
+        return $this->redirectToRoute('app_signalement_historique_view', ['uuid' => $signalement->getUuid()]);
     }
 }
