1ed9396e9d957aa87d2dbe88ddbd55ca6c4cd65e
Hélène Meneuvrier <hmeneuvrier@gmail.com>
2023-06-14 11:18:10 +0200

    318 : use HttpClientInterface

--- src/Command/GeolocateCommand.php
+++ src/Command/GeolocateCommand.php
@@ -4,23 +4,21 @@ namespace App\Command;
 
 use App\Entity\Signalement;
 use Doctrine\ORM\EntityManagerInterface;
-use GuzzleHttp\Client;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class GeolocateCommand extends Command
 {
     protected static $defaultName = 'app:geolocate';
 
-    private EntityManagerInterface $entityManager;
-    private Client $httpClient;
-
-    public function __construct(EntityManagerInterface $entityManager)
-    {
+    public function __construct(
+        private readonly HttpClientInterface $client,
+        private EntityManagerInterface $entityManager
+    ) {
         parent::__construct();
-        $this->entityManager = $entityManager;
-        $this->httpClient = new Client();
     }
 
     protected function configure()
@@ -40,31 +38,44 @@ class GeolocateCommand extends Command
             // Compose the address string to be used in the geocoding API request
             $fullAddress = $address.', '.$postalCode.' '.$city;
 
-            // Make the API request to geocode the address
-            $response = $this->httpClient->request('GET', 'https://api-adresse.data.gouv.fr/search', [
-                'query' => [
-                    'q' => $fullAddress,
-                    'limit' => 1,
-                ],
-            ]);
+            $statusCode = Response::HTTP_SERVICE_UNAVAILABLE;
+            try {
+                // Make the API request to geocode the address
+                $response = $this->client->request('GET', 'https://api-adresse.data.gouv.fr/search', [
+                    'query' => [
+                        'q' => $fullAddress,
+                        'limit' => 1,
+                    ],
+                ]);
 
-            $data = json_decode($response->getBody(), true);
+                $statusCode = $response->getStatusCode();
+                if (200 === $statusCode) {
+                    $data = json_decode($response->getContent(), true);
 
-            if (!empty($data['features'][0]['geometry']['coordinates'])) {
-                $coordinates = [
-                    'lat' => (string) $data['features'][0]['geometry']['coordinates'][1],
-                    'lng' => (string) $data['features'][0]['geometry']['coordinates'][0],
-                ];
+                    if (!empty($data['features'][0]['geometry']['coordinates'])) {
+                        $coordinates = [
+                            'lat' => (string) $data['features'][0]['geometry']['coordinates'][1],
+                            'lng' => (string) $data['features'][0]['geometry']['coordinates'][0],
+                        ];
 
-                // Update the geolocation coordinates in the signalement entity
-                $signalement->setGeoloc($coordinates);
+                        // Update the geolocation coordinates in the signalement entity
+                        $signalement->setGeoloc($coordinates);
 
-                $this->entityManager->persist($signalement);
-                $this->entityManager->flush();
+                        $this->entityManager->persist($signalement);
+                        $this->entityManager->flush();
 
-                $output->writeln('Geolocation updated for Signalement ID: '.$signalement->getId());
-            } else {
-                $output->writeln('Geolocation not found for Signalement ID: '.$signalement->getId());
+                        $output->writeln('Geolocation updated for Signalement ID: '.$signalement->getId());
+                    } else {
+                        $output->writeln('Geolocation not found for Signalement ID: '.$signalement->getId());
+                    }
+                } else {
+                    $output->writeln('Geolocation request failed for Signalement ID: '.$signalement->getId()
+                    .' statusCode = '.$statusCode);
+                }
+            } catch (\Throwable $exception) {
+                $output->writeln('Exception for Signalement ID: '.$signalement->getId()
+                .' statusCode = '.$statusCode
+                .' message = '.$exception->getMessage());
             }
         }
 
