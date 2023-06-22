89f9eeedd86db89e267a3af79441def84a8f1d70
Hélène Meneuvrier <hmeneuvrier@gmail.com>
2023-06-22 22:27:52 +0200

    320 : plug slider on controller

--- public/js/maps.js
+++ public/js/maps.js
@@ -11,6 +11,7 @@ var map = L.map('map-signalements-view', {
     zoom: 5
 });
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
+var heat;
 
 async function getMarkers(offset) {
     await fetch('?load_markers=true&offset=' + offset, {
@@ -18,9 +19,11 @@ async function getMarkers(offset) {
             'X-TOKEN': document.querySelector('#carto__js').getAttribute('data-token')
         },
         method: 'POST',
-        // body: new FormData(document.querySelector('form#bo_carto_filter'))
+        body: new FormData(document.querySelector('form#bo_carto_filter'))
     }).then(r => r.json().then(res => {
-        // let marker;
+        if (heat){
+            heat.remove();
+        }
         if (res.signalements) {  
 
             var heatValues = new Array();
@@ -28,27 +31,16 @@ async function getMarkers(offset) {
                 if (!isNaN(parseFloat(signalement.geoloc?.lng)) && !isNaN(parseFloat(signalement.geoloc?.lat))) {  
                     // pour l'instant on ne prend pas en compte le niveauInfestation
                     // on n'affiche pas les signalements au statut resolved
-
                     if ('trace' === signalement.statut){
                         heatValues.push([signalement.geoloc.lat, signalement.geoloc.lng, 0.3]);
                     }  
                     if ('en cours' === signalement.statut){
                         heatValues.push([signalement.geoloc.lat, signalement.geoloc.lng, 1]);
-
                     }         
-                    // heatValues.push([signalement.geoloc.lat, signalement.geoloc.lng, 10]);
-                    // virer le max, couleur à définir en fonction niveau intensité
-                    // gradient - color gradient config, e.g. {0.4: 'blue', 0.65: 'lime', 1: 'red'}
-
-                    // les couleurs peuvent etre
-                    // bleu transparent (comme le screenshot) pour les traces
-                    // rouges pour les infestations en cours
-
                 }
             })
 
-            var heat = L.heatLayer(heatValues, {radius: 25}).addTo(map);
-            // var heat = L.heatLayer(heatValues, {radius: 25, max:0.4, gradient:{0: 'blue', 0.1: 'lime', 0.2: 'orange', 0.3: 'red', 0.4: 'black'}}).addTo(map);
+            heat = L.heatLayer(heatValues, {radius: 25}).addTo(map);
         } else {
             alert('Erreur lors du chargement des signalements...')
         }
@@ -56,5 +48,5 @@ async function getMarkers(offset) {
 }
 
 window.onload = async () => {
-    await getMarkers(0);
+    // await getMarkers(0);
 }
