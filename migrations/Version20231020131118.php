1d67a8acbf9626d4d65bc0ea76bdb5a75c8b8b3e
Hélène Meneuvrier <hmeneuvrier@gmail.com>
2023-10-23 10:54:41 +0200

    372 : fix migration error

--- migrations/Version20231020131118.php
+++ migrations/Version20231020131118.php
@@ -16,13 +16,13 @@ final class Version20231020131118 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE entreprise_publique CHANGE adresse adresse VARCHAR(255) DEFAULT NULL ADD is_intervention TINYINT(1) NOT NULL, ADD is_detection_canine TINYINT(1) NOT NULL, ADD is_pro_only TINYINT(1) DEFAULT NULL');
+        $this->addSql('ALTER TABLE entreprise_publique CHANGE adresse adresse VARCHAR(255) DEFAULT NULL, ADD is_intervention TINYINT(1) NOT NULL, ADD is_detection_canine TINYINT(1) NOT NULL, ADD is_pro_only TINYINT(1) DEFAULT NULL');
         $this->addSql('UPDATE entreprise_publique SET is_intervention = 1 WHERE is_intervention = \'\'');
         $this->addSql('UPDATE entreprise_publique SET is_detection_canine = 0 WHERE is_detection_canine = \'\'');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE entreprise_publique CHANGE adresse adresse VARCHAR(255) NOT NULL DROP is_intervention, DROP is_detection_canine, DROP is_pro_only');
+        $this->addSql('ALTER TABLE entreprise_publique CHANGE adresse adresse VARCHAR(255) NOT NULL, DROP is_intervention, DROP is_detection_canine, DROP is_pro_only');
     }
 }
