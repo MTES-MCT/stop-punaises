7056cf052dc24d84b1512f153d35b91932c6e023
Hélène Meneuvrier <hmeneuvrier@gmail.com>
2023-10-26 11:53:31 +0200

    384 : add tests

---
+++ tests/Functionnal/Controller/SignalementViewControllerTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Tests\Functionnal\Controller;
+
+use App\Repository\InterventionRepository;
+use App\Repository\SignalementRepository;
+use App\Repository\UserRepository;
+use App\Tests\SessionHelper;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Routing\RouterInterface;
+
+class SignalementViewControllerTest extends WebTestCase
+{
+    use SessionHelper;
+
+    public function testSignalementSuccessfullyDisplay(): void
+    {
+        $client = static::createClient();
+        /** @var UserRepository $userRepository */
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        $user = $userRepository->findOneBy(['email' => 'admin@punaises.fr']);
+        /** @var SignalementRepository $signalementRepository */
+        $signalementRepository = static::getContainer()->get(SignalementRepository::class);
+        $signalement = $signalementRepository->findOneBy(['reference' => '2023-1']);
+
+        $client->loginUser($user);
+
+        /** @var RouterInterface $router */
+        $router = self::getContainer()->get(RouterInterface::class);
+        $routePostSignalement = $router->generate('app_signalement_view', ['uuid' => $signalement->getUuid()]);
+        $client->request('GET', $routePostSignalement);
+
+        $this->assertResponseIsSuccessful($signalement->getId());
+    }
+
+    public function testAcceptSignalement(): void
+    {
+        $client = static::createClient();
+
+        /** @var UserRepository $userRepository */
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        $user = $userRepository->findOneBy(['email' => 'company-01@punaises.fr']);
+        /** @var SignalementRepository $signalementRepository */
+        $signalementRepository = static::getContainer()->get(SignalementRepository::class);
+        $signalement = $signalementRepository->findOneBy(['reference' => '2023-1']);
+
+        $client->loginUser($user);
+
+        /** @var RouterInterface $router */
+        $router = self::getContainer()->get(RouterInterface::class);
+        $routePostSignalement = $router->generate(
+            'app_signalement_intervention_accept',
+            [
+                'uuid' => $signalement->getUuid(),
+                '_csrf_token' => $this->generateCsrfToken($client, 'signalement_intervention_accept'),
+            ]
+        );
+        $client->request('POST', $routePostSignalement);
+
+        $this->assertResponseRedirects('/bo/signalements/'.$signalement->getUuid());
+
+        /** @var InterventionRepository $interventionRepository */
+        $interventionRepository = static::getContainer()->get(InterventionRepository::class);
+
+        $intervention = $interventionRepository->findBy([
+            'signalement' => $signalement,
+            'entreprise' => $user->getEntreprise(),
+        ]);
+        $this->assertCount(1, $intervention);
+
+        // on rappelle la route une deuxième fois pour vérifier qu'une deuxième intervention n'est pas créée
+        $client->request('POST', $routePostSignalement);
+
+        $intervention = $interventionRepository->findBy([
+            'signalement' => $signalement,
+            'entreprise' => $user->getEntreprise(),
+        ]);
+        $this->assertCount(1, $intervention);
+    }
+}
