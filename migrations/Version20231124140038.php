fbdf0cc803cfabc9ed32bec916216c09af35e3ca
emilschn <emilien.schneider@beta.gouv.fr>
2023-11-24 14:06:15 +0100

    add migration to open 63 and 974 #474

---
+++ migrations/Version20231124140038.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20231124140038 extends AbstractMigration
+{
+    private const TERRITOIRES = [
+        '63', '974',
+    ];
+
+    public function getDescription(): string
+    {
+        return 'Open territories 63 and 974';
+    }
+
+    public function up(Schema $schema): void
+    {
+        foreach (self::TERRITOIRES as $zipCode) {
+            $this->addSql('UPDATE territoire SET active = 1 WHERE zip = :zip', ['zip' => $zipCode]);
+        }
+    }
+
+    public function down(Schema $schema): void
+    {
+        foreach (self::TERRITOIRES as $zipCode) {
+            $this->addSql('UPDATE territoire SET active = 0 WHERE zip = :zip', ['zip' => $zipCode]);
+        }
+    }
+}
