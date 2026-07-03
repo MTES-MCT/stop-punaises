f6e1c52035a00f2dc4b4910b3cd34e87796f2c99
numew <numabord@gmail.com>
2026-07-03 15:33:14 +0200

    Mettre en mode maintenance (#887)
    * mode maintenance #886
    
    * composer update #886

--- src/Controller/Front/SignalementTransportController.php
+++ src/Controller/Front/SignalementTransportController.php
@@ -17,6 +17,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SignalementTransportController extends AbstractController
 {
+    private bool $isSignalementsDisabled;
+
+    public function __construct(
+        ParameterBagInterface $parameterBag,
+    ) {
+        $this->isSignalementsDisabled = $parameterBag->get('is_signalements_disabled');
+    }
+
     #[Route(
         '/signalement/transport',
         name: 'app_front_signalement_transport',
@@ -24,6 +32,10 @@ class SignalementTransportController extends AbstractController
     )]
     public function index(ParameterBagInterface $parameterBag): Response
     {
+        if ($this->isSignalementsDisabled) {
+            return $this->redirectToRoute('home');
+        }
+
         if (!$parameterBag->get('feature_three_forms')) {
             return $this->redirectToRoute('home');
         }
@@ -44,6 +56,10 @@ class SignalementTransportController extends AbstractController
         ParameterBagInterface $parameterBag,
         GeolocateService $geolocateService,
     ): Response {
+        if ($this->isSignalementsDisabled) {
+            return $this->redirectToRoute('home');
+        }
+
         if (!$parameterBag->get('feature_three_forms')) {
             return $this->redirectToRoute('home');
         }
